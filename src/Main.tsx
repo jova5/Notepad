7e421c398b207e1968bb0139247a9ffd3342da09
Miloš Jovičić <jovicicm55@outlook.com>
2024-03-21 22:11:41 +0100

    working on selecting card

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,9 +1,101 @@
-import {FAB as Fab, Searchbar, useTheme} from 'react-native-paper';
-import {SafeAreaView, ScrollView, StyleSheet} from 'react-native';
-import React from 'react';
+import {
+  Button,
+  Card,
+  FAB as Fab,
+  Searchbar,
+  Text,
+  useTheme,
+  Avatar,
+} from 'react-native-paper';
+import {
+  FlatList,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  View,
+} from 'react-native';
+import React, {useEffect, useState} from 'react';
+
+const data = [
+  {id: 1, title: 'A', content: 'Random content'},
+  {id: 2, title: 'B', content: 'Some other content for B'},
+  {id: 3, title: 'C', content: 'Content for C'},
+  {id: 4, title: 'D', content: 'Additional content for D'},
+  {id: 5, title: 'E', content: 'Content for E goes here'},
+  {id: 6, title: 'F', content: 'Content for F'},
+  {id: 7, title: 'G', content: 'Last but not least, content for G'},
+  {id: 8, title: 'A', content: 'Random content'},
+  {
+    id: 9,
+    title: 'B',
+    content: 'Some other content for B dsa dasd asd asd asd as dasd asd as d',
+  },
+  {id: 10, title: 'C', content: 'Content for C'},
+  {id: 11, title: 'D', content: 'Additional content for D'},
+  {id: 12, title: 'E', content: 'Content for E goes here'},
+  {id: 13, title: 'F', content: 'Content for F'},
+  {id: 14, title: 'G', content: 'Last but not least, content for G'},
+  {id: 15, title: 'A', content: 'Random content'},
+  {id: 16, title: 'B', content: 'Some other content for B'},
+  {id: 17, title: 'C', content: 'Content for C'},
+  {id: 18, title: 'D', content: 'Additional content for D'},
+  {id: 19, title: 'E', content: 'Content for E goes here'},
+  {id: 20, title: 'F', content: 'Content for F'},
+  {id: 21, title: 'G', content: 'Last but not least, content for G'},
+  {id: 22, title: 'A', content: 'Random content'},
+  {
+    id: 23,
+    title: 'B',
+    content: 'Some other content for B dsa dasd asd asd asd as dasd asd as d',
+  },
+  {id: 24, title: 'C', content: 'Content for C'},
+  {id: 25, title: 'D', content: 'Additional content for D'},
+  {id: 26, title: 'E', content: 'Content for E goes here'},
+  {id: 27, title: 'F', content: 'Content for F'},
+  {id: 28, title: 'G', content: 'Last but not least, content for G'},
+];
+
+const RenderListItem = ({
+  item,
+  selectedIds,
+  onLongPress,
+  onPress,
+}: {
+  item: any;
+  selectedIds: any[];
+  onLongPress: (id: number) => void;
+  onPress: (id: number) => void;
+}) => {
+  if (item.empty) {
+    return <View style={[styles.item, styles.itemInvisible]} />;
+  }
+  const theme = useTheme();
+
+  return (
+    <Card
+      style={[
+        styles.item,
+        {
+          backgroundColor: selectedIds.includes(item.id)
+            ? theme.colors.elevation.level5
+            : theme.colors.elevation.level1,
+        },
+      ]}
+      onPress={() => onPress(item.id)}
+      onLongPress={() => onLongPress(item.id)}>
+      <Card.Content>
+        <Text variant="titleLarge">{item.title}</Text>
+        <Text variant="bodyMedium" numberOfLines={5}>
+          {item.content}
+        </Text>
+      </Card.Content>
+    </Card>
+  );
+};
 
 const Main = ({setOpen}: {setOpen: (prev: boolean) => void}) => {
   const theme = useTheme();
+  const [selectedIds, setSelectedIds] = useState([]);
 
   const [searchQuery, setSearchQuery] = React.useState('');
 
@@ -30,12 +122,53 @@ const Main = ({setOpen}: {setOpen: (prev: boolean) => void}) => {
         traileringIcon={'menu'}
         onTraileringIconPress={() => setOpen(true)}
       />
-      <ScrollView
-        showsVerticalScrollIndicator={false}
-        style={{
-          display: 'flex',
-        }}
-      />
+
+      <ScrollView scrollEnabled={true}>
+        <View style={{flexDirection: 'row'}}>
+          <View style={{flex: 1}}>
+            {data.map((item, index) => {
+              if (index % 2 === 0) {
+                return (
+                  <RenderListItem
+                    key={item.id}
+                    item={item}
+                    selectedIds={selectedIds}
+                    onLongPress={(id: number) =>
+                      setSelectedIds(prev => [...prev, id])
+                    }
+                    onPress={(id: number) =>
+                      setSelectedIds(prev =>
+                        prev.filter(prevId => prevId !== id),
+                      )
+                    }
+                  />
+                );
+              }
+            })}
+          </View>
+          <View style={{flex: 1}}>
+            {data.map((item, index) => {
+              if (index % 2 !== 0) {
+                return (
+                  <RenderListItem
+                    key={item.id}
+                    item={item}
+                    selectedIds={selectedIds}
+                    onLongPress={(id: number) =>
+                      setSelectedIds(prev => [...prev, id])
+                    }
+                    onPress={(id: number) =>
+                      setSelectedIds(prev =>
+                        prev.filter(prevId => prevId !== id),
+                      )
+                    }
+                  />
+                );
+              }
+            })}
+          </View>
+        </View>
+      </ScrollView>
     </SafeAreaView>
   );
 };
@@ -45,6 +178,7 @@ export default Main;
 const styles = StyleSheet.create({
   container: {
     display: 'flex',
+    flex: 1,
     flexGrow: 1,
     padding: 10,
   },
@@ -53,5 +187,12 @@ const styles = StyleSheet.create({
     margin: 16,
     right: 0,
     bottom: 0,
+    zIndex: 2,
+  },
+  item: {
+    margin: 5,
+  },
+  itemInvisible: {
+    backgroundColor: 'transparent',
   },
 });
