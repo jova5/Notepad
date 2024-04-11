0c66c22faa744d53df3775c1b993703c53789a17
Miloš Jovičić <jovicicm55@outlook.com>
2024-04-11 20:03:42 +0200

    still replacing text with div

--- src/text-editor/Editor.tsx
+++ src/text-editor/Editor.tsx
@@ -1,5 +1,5 @@
 import WebView from 'react-native-webview';
-import {forwardRef, Ref} from 'react';
+import {forwardRef, Ref, useRef} from 'react';
 import {useTheme} from 'react-native-paper';
 
 const myHtmlFile = require('./TextEditor.html');
@@ -7,11 +7,29 @@ const myHtmlFile = require('./TextEditor.html');
 const Editor = forwardRef((props, ref: Ref<WebView>) => {
   const theme = useTheme();
 
+  // const webViewRef = ref;
+  //
+  // const sendDataToWebView = () => {
+  //   const data = {key: 'value'}; // Your data object
+  //   const jsonData = JSON.stringify(data); // Convert data to JSON string
+  //
+  //   // Inject JavaScript code into the WebView to call the function with the data
+  //   ref.current.injectJavaScript(`receiveDataFromReactNative(${jsonData})`);
+  // };
+  //
+  // const onLoadWebView = () => {
+  //   sendDataToWebView(); // Send data after the WebView is loaded
+  // };
+
   return (
     <WebView
       ref={ref}
       originWhitelist={['*']}
       source={myHtmlFile}
+      javaScriptEnabled={true}
+      injectedJavaScriptBeforeContentLoaded={
+        'receiveDataFromReactNative("test")'
+      }
       style={{
         backgroundColor: theme.colors.background,
       }}
